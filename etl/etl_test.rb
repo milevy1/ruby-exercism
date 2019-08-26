f9545518d97691e5477430c745e19eaa934bd8aa
Matt Levy <milevy1@gmail.com>
2019-08-26 16:18:51 -0600

    Adds ETL transform solution

--- etl/etl_test.rb
+++ etl/etl_test.rb
@@ -1,10 +1,10 @@
 require 'minitest/autorun'
+require 'minitest/pride'
 require_relative 'etl'
 
 # Common test data version: 1.0.0 ca9ed58
 class EtlTest < Minitest::Test
   def test_a_single_letter
-    # skip
     old = {
       1 => ["A"]
     }
@@ -15,7 +15,6 @@ class EtlTest < Minitest::Test
   end
 
   def test_single_score_with_multiple_letters
-    skip
     old = {
       1 => ["A", "E", "I", "O", "U"]
     }
@@ -30,7 +29,6 @@ class EtlTest < Minitest::Test
   end
 
   def test_multiple_scores_with_multiple_letters
-    skip
     old = {
       1 => ["A", "E"],
       2 => ["D", "G"]
@@ -45,7 +43,6 @@ class EtlTest < Minitest::Test
   end
 
   def test_multiple_scores_with_differing_numbers_of_letters
-    skip
     old = {
       1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
       2 => ["D", "G"],
