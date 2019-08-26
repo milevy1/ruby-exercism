96db5157d8dc039dde498e51ca261dd00f6eaf97
Matt Levy <milevy1@gmail.com>
2019-08-26 16:36:24 -0600

    Adds solution to nucleotide_count

---
+++ nucleotide-count/nucleotide_count.rb
@@ -0,0 +1,28 @@
+class Nucleotide
+  def initialize(dna)
+    @dna = dna
+  end
+
+  def self.from_dna(dna)
+    raise ArgumentError if invalid_dna?(dna)
+
+    Nucleotide.new(dna)
+  end
+
+  def self.invalid_dna?(dna)
+    /[^AGCT]/.match?(dna)
+  end
+
+  def count(strand)
+    dna.split('').count(strand)
+  end
+
+  def histogram
+    ['A', 'T', 'C', 'G'].each_with_object({}) do |strand, result|
+      result[strand] = count(strand)
+    end
+  end
+
+  private
+  attr_reader :dna
+end
