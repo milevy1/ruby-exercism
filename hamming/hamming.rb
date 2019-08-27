9a6ea3275a334683294a6e421f4fa4c45073d4db
Matt Levy <milevy1@gmail.com>
2019-08-27 09:25:30 -0600

    Adds hamming iteration 2 replacing .reduce with .map

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -3,9 +3,8 @@ class Hamming
     raise ArgumentError if strand_1.length != strand_2.length
     return 0 if strand_1 == strand_2
 
-    (0..strand_1.length).reduce(0) do |acc, i|
-      acc += 1 if strand_1[i] != strand_2[i]
-      acc
-    end
+    (0..strand_1.length).map do |i|
+      strand_1[i] != strand_2[i] ? 1 : 0
+    end.sum
   end
 end
