517b417276ad6de6f8c7b102efccfdd9520d1f1d
Matt Levy <milevy1@gmail.com>
2019-09-05 09:44:36 -0600

    Refactors result.empty? and renames argument number to integer

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,10 +1,10 @@
 class Raindrops
-  def self.convert(number)
+  def self.convert(integer)
     result = ""
-    result << "Pling" if number % 3 == 0
-    result << "Plang" if number % 5 == 0
-    result << "Plong" if number % 7 == 0
+    result << "Pling" if integer % 3 == 0
+    result << "Plang" if integer % 5 == 0
+    result << "Plong" if integer % 7 == 0
 
-    result.empty? ? number.to_s : result
+    result.empty? ? integer.to_s : result
   end
 end
