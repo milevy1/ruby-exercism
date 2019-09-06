fa3e8905d4fa1af22f465b4904106736e5102aaf
Matt Levy <milevy1@gmail.com>
2019-09-06 17:03:02 -0600

    Adds iteration 1 solution to isogram

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,3 +1,11 @@
 class Isogram
-  
+  def self.isogram?(input)
+    letters = input.gsub(/[ -]/, '').downcase
+
+    letters.chars.each do |char|
+      return false if letters.count(char) > 1
+    end
+
+    true
+  end
 end
