746e1183e40f48a25583b1981e0599e327771817
Matt Levy <milevy1@gmail.com>
2019-09-06 16:38:07 -0600

    Refactors raindrops to private and public methods

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,32 +1,34 @@
 class Raindrops
-  def initialize(integer)
-    @integer = integer
-  end
-
-  def self.convert(integer)
-    new(integer).to_s
-  end
-
-  def to_s
-    raindrop_speak.empty? ? integer.to_s : raindrop_speak
-  end
-
   RULES = {
     3 => "Pling",
     5 => "Plang",
     7 => "Plong"
   }
 
-  def raindrop_speak
-    RULES.each_with_object(result = '') do |(divisor, rain_sound), result|
-      result << rain_sound if divisible?(divisor)
-    end
-  end
-
-  def divisible?(divisor)
-    integer % divisor == 0
+  def self.convert(integer)
+    new(integer).to_s
   end
 
   private
   attr_reader :integer
+
+  def initialize(integer)
+    @integer = integer
+  end
+
+  def raindrop_speak
+    RULES.each_with_object(result = '') do |(divisor, sound), result|
+      result << sound if divisible_by?(divisor)
+    end
+  end
+
+  def divisible_by?(divisor)
+    integer % divisor == 0
+  end
+
+  public
+
+  def to_s
+    raindrop_speak.empty? ? integer.to_s : raindrop_speak
+  end
 end
