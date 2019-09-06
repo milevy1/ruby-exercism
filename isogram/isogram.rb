8b73d3db80f1c8fd2fe591374aa3a738bcb54991
Matt Levy <milevy1@gmail.com>
2019-09-06 17:08:39 -0600

    Refactors isogram into SRP methods

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,9 +1,28 @@
 class Isogram
   def self.isogram?(input)
-    letters = input.gsub(/[ -]/, '').downcase
+    new(input).isogram?
+  end
 
-    letters.chars.each do |char|
-      return false if letters.count(char) > 1
+  private
+  
+  attr_reader :input
+  def initialize(input)
+    @input = input
+  end
+
+  def letters
+    input.gsub(/[ -]/, '').downcase
+  end
+
+  def letter_repeats?(letter)
+    letters.count(letter) > 1
+  end
+
+  public
+
+  def isogram?
+    letters.chars.each do |letter|
+      return false if letter_repeats?(letter)
     end
 
     true
