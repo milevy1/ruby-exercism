fc8a10952b57b481bb52f945ea545ed956402714
Matt Levy <milevy1@gmail.com>
2019-09-06 14:15:52 -0600

    Refactors self.convert into instance methods

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,15 +1,32 @@
 class Raindrops
+  def initialize(integer)
+    @integer = integer
+  end
+
+  def self.convert(integer)
+    new(integer).to_s
+  end
+
+  def to_s
+    raindrop_speak.empty? ? integer.to_s : raindrop_speak
+  end
+
   RULES = {
     3 => "Pling",
     5 => "Plang",
     7 => "Plong"
   }
 
-  def self.convert(integer)
-    RULES.each_with_object(result = '') do |(divisor, rain_sound), accumulator|
-      accumulator << rain_sound if integer % divisor == 0
+  def raindrop_speak
+    RULES.each_with_object(result = '') do |(divisor, rain_sound), result|
+      result << rain_sound if divisible?(divisor)
     end
-
-    result.empty? ? integer.to_s : result
   end
+
+  def divisible?(divisor)
+    integer % divisor == 0
+  end
+
+  private
+  attr_reader :integer
 end
